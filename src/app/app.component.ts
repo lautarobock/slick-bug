44e36e5924c70bc086bac41dfb609a05e6488ad9
Lautaro Cozzani <lautaromail@gmail.com>
2018-01-05 10:27:28 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  slides = [
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/333333"},
+    {img: "http://placehold.it/350x150/666666"},
+    {img: "http://placehold.it/350x150/999999"},
+    {img: "http://placehold.it/350x150/CCCCCC"},
+    {img: "http://placehold.it/350x150/FFFFFF"}
+  ];
+  slideConfig = {
+
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev" style="color: black; font-size:40px;z-index: 2000; left: -11px" (click)="slickModal.slickPrev()"><i class="fa fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next" style="color: black; font-size:40px; right: 13px" (click)="slickModal.slickNext()"><i class="fa fa-chevron-right"></i></button>',
+    edgeFriction: 0
+  };
+
+  addSlide() {
+    this.slides.push({img: "http://placehold.it/350x150/777777"})
+  }
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
+  afterChange(e) {
+    console.log('afterChange');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SlickModule } from 'ngx-slick';
 
 import { AppComponent } from './app.component';
+import { AnotherComponent } from './another/another.component';
 
+const appRoutes: Routes = [
+  { path: 'another', component: AnotherComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnotherComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes, {useHash: false}),
+    BrowserModule,
+    SlickModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
